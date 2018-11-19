37de077ffc33bc2bcddf9201b2df541788cc22f6
Fabien MARTY <fabien.marty@gmail.com>
2018-11-19 13:26:20 +0100

    feat: better batch_psql function
    handle bash errexit mode and add a way to override user/dbname

--- adm/sqltools.sh
+++ adm/sqltools.sh
@@ -1,13 +1,15 @@
 function batch_psql() {
     SQLFILE=$1
     INFO=${2:-}
+    DBNAME=${3:-${MFBASE_POSTGRESQL_DBNAME}}
+    USERNAME=${4:-${MFBASE_POSTGRESQL_USERNAME}}
     if test "${INFO}" = ""; then
         INFO="Playing ${SQLFILE}"
     fi
     echo -n "- ${INFO}..."
     echo_running
-    psql -U "${MFBASE_POSTGRESQL_USERNAME}" -h "${MODULE_RUNTIME_HOME}/var" -p "${MFBASE_POSTGRESQL_PORT}" -f "${SQLFILE}" -v "ON_ERROR_STOP=1" "${MFBASE_POSTGRESQL_DBNAME}" >"/tmp/psql.$$" 2>&1
-    if test $? -eq 0; then
+    ! psql -U "${USERNAME}" -h "${MODULE_RUNTIME_HOME}/var" -p "${MFBASE_POSTGRESQL_PORT}" -f "${SQLFILE}" -v "ON_ERROR_STOP=1" "${DBNAME}" >"/tmp/psql.$$" 2>&1
+    if test $? -ne 0; then
         echo_ok
         rm -f "/tmp/psql.$$"
         return 0
