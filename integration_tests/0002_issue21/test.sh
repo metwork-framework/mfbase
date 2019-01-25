dc668f36d7c4ab417671005b6ce095bc16eec17a
thebaptiste <jbaptiste31@free.fr>
2019-01-25 10:56:26 +0000

    feat: execute integration tests directly from mfbase module and lauch them on a pull request on the module

--- integration_tests/0002_issue21/test.sh
+++ integration_tests/0002_issue21/test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+plugins.uninstall foobar >/dev/null 2>&1
+rm -Rf foobar*
+
 set -x
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mfbase.start
+bootstrap_plugin.py create --no-input --make --install --delete foobar
 
-su --command="${DIR}/_test.sh" - mfbase
+source "${MFBASE_HOME}/share/sqltools.sh"
+
+cat >/tmp/postgis.sql <<EOF
+SELECT ST_IsValid(ST_GeomFromText('LINESTRING(0 0, 1 1)')) As good_line,
+        ST_IsValid(ST_GeomFromText('POLYGON((0 0, 1 1, 1 2, 1 1, 0 0))')) As bad_poly;
+EOF
+batch_psql "/tmp/postgis.sql" "test postgis" plugin_foobar plugin_foobar
+
+plugins.uninstall foobar
+mfbase.stop
+rm -Rf foobar*
