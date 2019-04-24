86edd7c5a1db8c3bfafc19463d8addb780a83547
Fabien MARTY <fabien.marty@gmail.com>
2019-04-24 10:12:07 +0200

    fix: better postgresql status during startup (on slow systems)

--- adm/_wait_postgresql.py
+++ adm/_wait_postgresql.py
@@ -3,23 +3,35 @@
 import sys
 import os
 import psycopg2
+import datetime
 
 USERNAME = os.environ["MFBASE_POSTGRESQL_USERNAME"]
 PASSWORD = os.environ["MFBASE_POSTGRESQL_PASSWORD"]
 HOST = "localhost"
 DBNAME = "postgres"
 PORT = int(os.environ["MFBASE_POSTGRESQL_PORT"])
+TIMEOUT = 30
 
-try:
-    conn = psycopg2.connect(host=HOST, port=PORT, user=USERNAME, dbname=DBNAME,
-                            password=PASSWORD, connect_timeout=10)
-    cur = conn.cursor()
-    cur.execute("SELECT current_timestamp")
-    if (cur.fetchall()[0][0].year >= 1900):
+
+def pgping(host, port, user, dbname, password, connect_timeout=10):
+    try:
+        conn = psycopg2.connect(host=host, port=port, user=user,
+                                dbname=dbname, password=password,
+                                connect_timeout=connect_timeout)
+        cur = conn.cursor()
+        cur.execute("SELECT current_timestamp")
+        if (cur.fetchall()[0][0].year >= 1900):
+            return True
+    except Exception as e:
+        return False
+
+
+first = datetime.datetime.now()
+while (datetime.datetime.now() - first).total_seconds() <= TIMEOUT:
+    res = pgping(HOST, PORT, USERNAME, DBNAME, PASSWORD)
+    if res:
         print("ok")
         sys.exit(0)
-except Exception as e:
-    pass
 
 print("not connected")
 sys.exit(1)
