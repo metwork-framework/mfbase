c001844ef6305ee200b5c1d542cda60eb62a0e46
metworkbot <marty.fabien@orange.fr>
2020-03-30 15:04:23 +0000

    chore: sync common files from resources repository

--- integration_tests/0096_system_extra_dependencies/test.sh
+++ integration_tests/0096_system_extra_dependencies/test.sh
@@ -14,26 +14,30 @@ RET=0
 
 
 cd "${MFMODULE_HOME}" || exit 1
-DEPS1=$(external_dependencies.sh |awk -F '/' '{print $NF}' |xargs)
-DEPS2=$(external_dependencies_not_found.sh |xargs)
-DEPS=$(echo $DEPS1 $DEPS2)
-for DEP in ${DEPS}; do
-    FOUND=0
-    for OK_DEP in ${OK_DEPS}; do
-        if test "${DEP}" = "${OK_DEP}"; then
-            FOUND=1
-            break
+cd opt
+for layer in `ls`; do
+    cd "${layer}"
+    DEPS1=$(layer_wrapper --layers=${layer}@${MFMODULE_LOWERCASE} -- external_dependencies.sh |awk -F '/' '{print $NF}' |xargs)
+    DEPS2=$(layer_wrapper --layers=${layer}@${MFMODULE_LOWERCASE} -- external_dependencies_not_found.sh |xargs)
+    DEPS=$(echo $DEPS1 $DEPS2)
+    for DEP in ${DEPS}; do
+        FOUND=0
+        for OK_DEP in ${OK_DEPS}; do
+            if test "${DEP}" = "${OK_DEP}"; then
+                FOUND=1
+                break
+            fi
+        done
+        if test "${FOUND}" = "1"; then
+            continue
         fi
+        echo "***** ${DEP} *****"
+        echo "=== revert ldd ==="
+        revert_ldd.sh "${DEP}"
+        echo
+        echo
+        RET=1
     done
-    if test "${FOUND}" = "1"; then
-        continue
-    fi
-    echo "***** ${DEP} *****"
-    echo "=== revert ldd ==="
-    revert_ldd.sh "${DEP}"
-    echo
-    echo
-    RET=1
 done
 
 
