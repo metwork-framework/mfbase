7b00760b4edbd5329cbf5d04c1dfbe36594b6bd3
MARTY FABIEN <fabien.marty@meteo.fr>
2021-01-28 16:45:41 +0100

    fix: fix some stop/start issues

--- adm/_wait_postgresql.py
+++ adm/_wait_postgresql.py
@@ -4,6 +4,8 @@ import sys
 import os
 import psycopg2
 import datetime
+import time
+from mfutil.cli import MFProgress
 
 USERNAME = os.environ["MFBASE_POSTGRESQL_USERNAME"]
 PASSWORD = os.environ["MFBASE_POSTGRESQL_PASSWORD"]
@@ -22,16 +24,22 @@ def pgping(host, port, user, dbname, password, connect_timeout=10):
         cur.execute("SELECT current_timestamp")
         if (cur.fetchall()[0][0].year >= 1900):
             return True
-    except Exception as e:
+    except Exception:
         return False
 
 
-first = datetime.datetime.now()
-while (datetime.datetime.now() - first).total_seconds() <= TIMEOUT:
-    res = pgping(HOST, PORT, USERNAME, DBNAME, PASSWORD)
-    if res:
-        print("ok")
-        sys.exit(0)
-
-print("not connected")
-sys.exit(1)
+with MFProgress() as progress:
+    t = progress.add_task("- Waiting for postgresql to be ready...", total=TIMEOUT)
+    before = datetime.datetime.now()
+    after = datetime.datetime.now()
+    while (after - before).total_seconds() < TIMEOUT:
+        res = pgping(HOST, PORT, USERNAME, DBNAME, PASSWORD)
+        if res:
+            progress.complete_task(t)
+            sys.exit(0)
+        time.sleep(1)
+        after = datetime.datetime.now()
+        delta = (after - before).total_seconds()
+        progress.update(t, completed=int(delta))
+    progress.complete_task_nok(t)
+    sys.exit(1)
