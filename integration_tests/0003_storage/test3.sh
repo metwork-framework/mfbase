b4c4072890ade47d58c58bc5fbfb172fc75b6464
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-01-16 13:20:43 +0000

    build: fix tests

---
+++ integration_tests/0003_storage/test3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+plugins.uninstall foobar4 >/dev/null 2>&1
+rm -Rf foobar4
+
+set -x
+set -e
+
+#Test if storage_dav_access is missing
+rm -Rf foobar4*
+bootstrap_plugin.py create --no-input foobar4
+cd foobar4
+cat config.ini | grep -v storage_dav_access > config.ini.new
+mv config.ini.new config.ini
+rm -f perm_prev.txt
+for l in "user:rw-" "group:---" "all:---"; do
+echo $l >> perm_prev.txt
+done
+make release
+plugins.install ./foobar4*plugin
+
+I=0
+rm -f foo4
+while test $I -lt 20; do
+    wget -O foo4 "http://localhost:${MFBASE_NGINX_PORT}/storage/foobar4/" || true
+    if test -f foo4; then
+        break
+    fi
+    I=$(expr ${I} + 1)
+    sleep 1
+done
+if test $I -ge 20; then
+    echo "ERROR: can't get http://localhost:${MFBASE_NGINX_PORT}/storage/foobar4/"
+    exit 1
+fi
+
+rm -f foo4
+curl -v -XPUT --data-binary @config.ini "http://localhost:${MFBASE_NGINX_PORT}/storage/foobar4/foo4"
+getfacl ${MFMODULE_RUNTIME_HOME}/var/storage/foobar4/foo4 | grep "::" > perm.txt
+diff perm.txt perm_prev.txt
+rm -f perm*.txt
+plugins.uninstall foobar4
+
+cd ..
+rm -Rf foobar4*
+exit 0
