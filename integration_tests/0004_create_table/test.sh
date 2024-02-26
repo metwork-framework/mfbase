901516287687292f12c6035d6cfbc7c0d72b6185
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-02-26 10:26:34 +0000

    build: fix

--- integration_tests/0004_create_table/test.sh
+++ integration_tests/0004_create_table/test.sh
@@ -10,11 +10,11 @@ bootstrap_plugin.py create --no-input --make --install --delete foobar
 
 source "${MFBASE_HOME}/share/sqltools.sh"
 
-cat >/tmp/create_base.sql <<EOF
+cat >/tmp/create_table.sql <<EOF
 CREATE TABLE foo(id int);
 EOF
 batch_psql "/tmp/create_table.sql" "test create table" plugin_foobar plugin_foobar
 
 plugins.uninstall foobar
-rm -f /tmp/create_base.sql
+rm -f /tmp/create_table.sql
 rm -Rf foobar*
